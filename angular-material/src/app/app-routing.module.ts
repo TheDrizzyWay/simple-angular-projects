3e0966e9936a9eb75b04692d784a9f8452e9236a
Christopher Iliyas <christopher.iliyas@andela.com>
2019-06-13 23:00:44 +0100

    import material components and modules

--- angular-material/src/app/app-routing.module.ts
+++ angular-material/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerCreateComponent } from './customer-create/customer-create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'customer-list', component: CustomerListComponent },
+  { path: 'customer-create', component: CustomerCreateComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-material/src/app/app.module.ts
+++ angular-material/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MyMaterialModule } from './material.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { CustomerListComponent } from './customer-list/customer-list.component';
@@ -16,6 +18,8 @@ import { CustomerCreateComponent } from './customer-create/customer-create.compo
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MyMaterialModule,
     AppRoutingModule
   ],
   providers: [],
