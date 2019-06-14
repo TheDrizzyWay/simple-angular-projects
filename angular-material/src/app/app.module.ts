3bb3dc57685aba547371d3eb4fe958875d9e71f6
Christopher Iliyas <christopher.iliyas@andela.com>
2019-06-14 05:31:17 +0100

    create error modal

--- angular-material/src/app/app.module.ts
+++ angular-material/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { MyMaterialModule } from './material.module';
@@ -8,20 +9,24 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { CustomerListComponent } from './customer-list/customer-list.component';
 import { CustomerCreateComponent } from './customer-create/customer-create.component';
+import { ErrorComponent } from './error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     CustomerListComponent,
-    CustomerCreateComponent
+    CustomerCreateComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MyMaterialModule,
+    FormsModule,
     AppRoutingModule
   ],
+  entryComponents: [ErrorComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
--- angular-material/src/app/login/login.component.ts
+++ angular-material/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from  '@angular/material';
+import { Router } from '@angular/router';
+import { ErrorComponent } from '../error.component';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  public username:string
+  public password:string
+  constructor(
+    private dialog: MatDialog,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
+
+  login() {
+        if(this.username === "demo" && this.password === "demo")
+        {
+            this.router.navigate(['customer-list']);
+        }
+        else {
+            this.dialog.open(ErrorComponent, {
+            data: { message:  "Your login information is incorrect!" }
+          });
+        }
+    }
 
 }
