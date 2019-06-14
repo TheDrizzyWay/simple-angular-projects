094f85a575ea1db653769aa4f33c87cdad2af166
Christopher Iliyas <christopher.iliyas@andela.com>
2019-06-14 23:09:47 +0100

    finish customer list component

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # simple-angular-projects
 1. A simple todo app based on an angular crash course by Brad Traversy
-2. A content management system based on this tutorial on pluralsight.
+2. A content management system based on this tutorial from pluralsight.
+3. A simple ui built with angular material based on a sitepoint tutorial
--- angular-material/src/app/customer-list/customer-list.component.ts
+++ angular-material/src/app/customer-list/customer-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatPaginator, MatSnackBar } from '@angular/material';
 import { Customer } from '../customer';
 
 @Component({
@@ -10,16 +10,37 @@ import { Customer } from '../customer';
 export class CustomerListComponent implements OnInit {
    loading: boolean = true;
    customers: Customer[] = [
-    { id:1, name:'Customer 001', job: 'Programmer' },
+    { id:1, name: 'Customer 001', job: 'Programmer' },
     { id:2, name: 'Customer 002', job: 'Writer' },
-    { id:3, name:'Customer 003', job:'None'}
+    { id:3, name: 'Customer 003', job: 'None' },
+    { id:4, name: 'Customer 004', job: 'Programmer' },
+    { id:5, name: 'Customer 005', job: 'Programmer' },
+    { id:6, name: 'Customer 006', job: 'Programmer' },
+    { id:7, name: 'Customer 007', job: 'Programmer' },
+    { id:8, name: 'Customer 008', job: 'Programmer' },
+    { id:9, name: 'Customer 009', job: 'Programmer' },
+    { id:10, name: 'Customer 010', job:'Programmer' },
     ];
    dataSource = new MatTableDataSource<Customer>(this.customers);
-   displayedColumns  = ['id', 'name', 'job'];
+   displayedColumns  = ['id', 'name', 'job', 'operations'];
+   pageSize = 5;
 
-  constructor() { }
+   @ViewChild(MatPaginator) paginator: MatPaginator;
 
+  constructor(public snackBar: MatSnackBar) { }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
   ngOnInit():void {
     setTimeout(() => this.loading = false, 2000);
+  }
+
+  deleteCustomer(id) {
+    let snackBarRef = this.snackBar.open(`Deleting customer #${id}`);
+  }
+
+  editCustomer(id) {
+    let snackBarRef = this.snackBar.open(`Editing customer #${id}`);
   }
 }
