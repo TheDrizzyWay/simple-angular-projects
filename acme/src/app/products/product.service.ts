719a8e7f2ce7d5e3cfb7565682642a01cacf4272
Christopher Iliyas <christopher.iliyas@andela.com>
2019-06-16 17:46:26 +0100

    add authentication guard

--- acme/src/app/products/product.service.ts
+++ acme/src/app/products/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 import { Iproduct } from './product';
